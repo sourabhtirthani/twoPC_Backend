af67c7f8efd1b6e1f0dbc53d682d519f5d56be88
dotcreativetech <dotcreativetech@gmail.com>
2026-01-03 12:58:54 +0530

    added the feature env feature

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,11 +6,12 @@ import { UserModule } from './user/user.module';
 import { IcoModule } from './ico/ico.module';
 import { TransactionModule } from './transaction/transaction.module';
 import { StakingModule } from './staking/staking.module';
-
+import * as dotenv from 'dotenv';
+dotenv.config();
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
-    MongooseModule.forRoot("mongodb+srv://sourabhtirthani:sourabhtirthani@cluster0.wpdlmbh.mongodb.net/two-pc-ico"),
+    MongooseModule.forRoot(`${process.env.MONGO_URI}`),
     UserModule,
     IcoModule,
     TransactionModule,
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import * as dotenv from 'dotenv';
+dotenv.config();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:3000', // Replace with your frontend URL
+    origin: '*', // Replace with your frontend URL
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
